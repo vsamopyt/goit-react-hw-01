493a2f846e77ed1b78c045f1b230d32a0336806c
v.samopyt <v.samopyt14@gmail.com>
2024-06-14 16:26:07 +0300

    commit-03
    correcting mistakes in FriendListItem.jsx and FriendListItem.module.css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,5 @@
-import clsx from "clsx";
-import css from "./FriendListItem.module.css"
+import clsx from 'clsx';
+import css from './FriendListItem.module.css';
 
 // export default function FriendListItem ({ avatar, name, isOnline }) {
 //   return (
@@ -11,14 +11,25 @@ import css from "./FriendListItem.module.css"
 //   );
 // }
 
+export default function FriendListItem({ item: { avatar, name, isOnline } }) {
+  return (
+    <>
+      <img
+        className={clsx(css['friendListItem-img'])}
+        src={avatar}
+        alt="avatar"
+      />
+      <h3 className={clsx(css['friendListItem-title'])}>{name}</h3>
 
-
-export default function FriendListItem ({item:{ avatar, name, isOnline }}) {
-    return (
-      <>
-        <img className ={clsx(css["friendListItem-img"])}  src={avatar} alt="avatar" />
-        <h3 className ={clsx(css["friendListItem-title"])}>{name}</h3>
-       <p className ={clsx(css["friendListItem-text"], css[isOnline])}> {isOnline? "Online":"Offline"}</p>
-      </>
-    );
-  }
+      <p
+        className={clsx(css['friendListItem-text'], {
+          [css['status-online']]: isOnline,
+          [css['status-offline']]: !isOnline,
+        })}
+      >
+        {' '}
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
+    </>
+  );
+}
